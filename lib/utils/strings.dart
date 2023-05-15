a874b6841bc1e4f8bc1e894419775b448c3d6036
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2023-05-15 17:02:18 -0300

    upload app

---
+++ lib/utils/strings.dart
@@ -0,0 +1,17 @@
+class MyString {
+  static const String mainTitle = "Tarefas";
+  static const String deletedTask = "Excluir tarefa";
+  static const String doneAllTask = "Você concluiu todas as tarefas!";
+  static const String addNewTask = "Adicionar ";
+  static const String updateCurrentTask = "Atualizar ";
+  static const String taskStrnig = "Tarefa";
+  static const String titleOfTitleTextField = "O que você está planejando?";
+  static const String addNote = 'Adicionar nota';
+  static const String timeString = "Tempo";
+  static const String dateString = "Data";
+  static const String deleteTask = "Deletar";
+  static const String addTaskString = "Adicionar tarefa";
+  static const String updateTaskString = "Atualizar";
+  static const String oopsMsg = "Oops!";
+  static const String areYouSure = "Você tem certeza?";
+}
